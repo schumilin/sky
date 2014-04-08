600a11471345ae080c72be90ea2fce7d81012e2e
zhailin <schumilin@163.com>
2014-04-08 17:02:57 +0800

    update admin logic

--- admin/www/js/index.js
+++ admin/www/js/index.js
@@ -50,29 +50,10 @@ var app = {
 
 AV.initialize("2uu9d14470rpv39bb1178vsddmkdfgis13zfr2be0vyeuog8", "o33s1rvaukqedeforme8f10wegjv69rdw0wjoei2cuka4u9q");
 
-$('.save-guess').on('click', function () {
-    var first = $('.first').val();
-    var second = $('.second').val();
-
-    var father = AV.Object.extend('guess');
-    var son = new father();
-    son.set('first', first);
-    son.set('second', second);
-    son.save(null, {
-      success: function(data) {
-        $('.car').html('预测数据保存成功.');
-      },
-      error: function(data, error) {
-        $('.car').html('预测数据保存失败.');
-      }
-    });
-});
-
-$('.fuck').on('click', function () {
-    fuckData();
-});
+var timer = 0;
 
 function fuckData () {
+
     // fuck pm25.in 
     $.ajax({
         dataType: "jsonp",
@@ -91,54 +72,88 @@ function fuckData () {
 
         son.save(null, {
             success: function(data) {
-                $('.car').html('数据爬取成功,并已经存入数据库.');
+                $('.car').append('<br>pm25.in 爬取成功,并已经存入数据库.');
             },
             error: function(data, error) {
-                $('.car').html('pm25.in 保存失败.');
+                $('.car').append('<br>pm25.in 存入数据库失败.');
             }
         });
 
     }).fail(function() {
-        $('.car').html('pm25.in 爬取失败.');
+        $('.car').append('<br>pm25.in 爬取失败.');
     });
     // fuck young-0
     var array = [];
     var domArray = $('#alonso').contents().find('.gr2');
 
-    domArray.each(function (index, el) {
-        array.unshift(parseInt($(el).text()));
-    });
+    if (domArray.length !== 0) {
+            domArray.each(function (index, el) {
+            array.unshift(parseInt($(el).text(), 10));
+        });
 
-    var father = AV.Object.extend('aqiChart');
-    var son = new father();
+        var father = AV.Object.extend('aqiChart');
+        var son = new father();
 
-    son.set('data', array);
+        son.set('data', array);
 
-    son.save(null, {
-        success: function(data) {
-            $('.car').html('数据爬取成功,并已经存入数据库.');
-        },
-        error: function(data, error) {
-            $('.car').html('young-0 爬取失败.');
-        }
-    });
+        son.save(null, {
+            success: function(data) {
+                $('.car').append('<br>young-0 数据爬取成功,并已经存入数据库.');
+            },
+            error: function(data, error) {
+                $('.car').append('<br>young-0 存入数据库失败.');
+            }
+        });
+    }
 }
 
-function createIframe () {
+function runCrawler () {
+
     $('iframe').remove();
+    $('.car').html('Console:');
+
     var iframe = document.createElement('iframe');
+
+    document.body.appendChild(iframe);
     iframe.setAttribute('id', 'alonso');
     iframe.src = 'http://www.young-0.com/airquality/index.php';
+
     iframe.onload = function(){
         fuckData();
     };
-    document.body.appendChild(iframe);
 }
 
-setInterval(function () {
-    createIframe();
-}, 1200000);
+function initCrawlerTimer () {
 
-$(document).ready(function() {
-    createIframe();
+    runCrawler();
+
+    clearInterval(timer);
+    timer = 0;
+
+    timer = setInterval(function () {
+        runCrawler();
+    }, 1200000);
+}
+
+$('.fuck').on('click', function () {
+    initCrawlerTimer();
+});
+
+// set guess info
+$('.save-guess').on('click', function () {
+    var first = $('.first').val();
+    var second = $('.second').val();
+
+    var father = AV.Object.extend('guess');
+    var son = new father();
+    son.set('first', first);
+    son.set('second', second);
+    son.save(null, {
+        success: function(data) {
+        $('.car').append('<br>预测数据保存成功.');
+        },
+        error: function(data, error) {
+        $('.car').append('<br>预测数据保存失败.');
+        }
+    });
 });
